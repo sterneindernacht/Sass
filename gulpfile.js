40afb76a15fe768b7932185df36661e8575855b4
Anita <kaczmarek.anita0@gmail.com>
2021-05-11 22:44:09 +0200

    1

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+gulp.task("sass"
+
+, function() {
+
+return gulp.src("scss/style.scss")
+.pipe(sass().on("error"
+
+, sass.logError))
+
+.pipe(gulp.dest("css"))
+
+});
+
