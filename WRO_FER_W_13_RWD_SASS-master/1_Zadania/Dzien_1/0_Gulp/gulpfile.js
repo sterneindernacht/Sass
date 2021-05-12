07d7b549da609dc16ec4cabd48aa855aa7ed0b21
Anita <kaczmarek.anita0@gmail.com>
2021-05-12 22:45:18 +0200

    sass

---
+++ WRO_FER_W_13_RWD_SASS-master/1_Zadania/Dzien_1/0_Gulp/gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('scss/main.scss')
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest('css'))
+})
+
+gulp.task('watch', function () {
+    gulp.watch('scss/*.scss', gulp.series("sass"));
+})  
